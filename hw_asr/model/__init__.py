fd58a5535cd73e95873fdaed76b82fada1ce53c8
nina <67192642+quynhu-d@users.noreply.github.com>
2021-10-16 19:06:44 +0300

    typo in model/__init__ removed

--- hw_asr/model/__init__.py
+++ hw_asr/model/__init__.py
@@ -1,5 +1,5 @@
 from hw_asr.model.baseline_model import BaselineModel
-from hw_ast.model.lstm_model import LSTMModel
+from hw_asr.model.lstm_model import LSTMModel
 
 __all__ = [
     "BaselineModel",
