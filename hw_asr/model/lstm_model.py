cf4735937893230cde4ead6fe016413c6357b4b4
nina <67192642+quynhu-d@users.noreply.github.com>
2021-10-16 20:46:30 +0300

    added padding in lstm model

--- hw_asr/model/lstm_model.py
+++ hw_asr/model/lstm_model.py
@@ -7,22 +7,15 @@ from hw_asr.base import BaseModel
 class LSTMModel(BaseModel):
     def __init__(self, n_feats, n_class, fc_hidden=512, *args, **kwargs):
         super().__init__(n_feats, n_class, *args, **kwargs)
-        self.rnn = nn.LSTM(input_size=n_feats, hidden_size=fc_hidden, batch_first=True)
+        self.rnn = nn.LSTM(input_size=n_feats, num_layers=3, hidden_size=fc_hidden, batch_first=True)
         self.fc_hidden = fc_hidden
         self.n_feats = n_feats
-        self.clf = Sequential(
-            nn.Linear(fc_hidden, fc_hidden),
-            nn.ReLU(),
-            nn.Linear(fc_hidden, n_class)
-        )
+        self.clf = nn.Linear(fc_hidden, n_class)
 
     def forward(self, spectrogram, spectrogram_length, *args, **kwargs):
-
-        output, _ = self.rnn(spectrogram)
-
-        # Now we want to take the last hidden state of each instance in batch
-        # BUT we don't want to take `padding` hidden state
-        # We will use `torch.gather` and `length` to do that
+        padded_input = nn.utils.rnn.pack_padded_sequence(
+            spectrogram, spectrogram_length, batch_first=True, enforce_sorted=False)
+        output, _ = self.rnn(padded_input)
 
         # learn more about gather
         # https://medium.com/analytics-vidhya/understanding-indexing-with-pytorch-gather-33717a84ebc4
@@ -34,7 +27,7 @@ class LSTMModel(BaseModel):
         # )
         # print('last_hidden shape', last_hidden.shape)
 
-        logits = self.clf(output)
+        logits = self.clf(nn.utils.rnn.pad_packed_sequence(output, batch_first=True)[0])
         return {"logits": logits}
 
     def transform_input_lengths(self, input_lengths):
