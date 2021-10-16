c0964d2e05703a265c3913cef6eaf18fded7175c
nina <67192642+quynhu-d@users.noreply.github.com>
2021-10-16 13:14:09 +0300

    Update collate.py

--- hw_asr/collate_fn/collate.py
+++ hw_asr/collate_fn/collate.py
@@ -9,6 +9,24 @@ def collate_fn(dataset_items: List[dict]):
     Collate and pad fields in dataset items
     """
 
-    result_batch = {}
-    # TODO: your code here
-    raise NotImplementedError
+    bs = len(dataset_items)
+    spec_ft_s = dataset_items[0]['spectrogram'].squeeze().shape[0]
+
+    text_time_lengths = [item['text_encoded'].squeeze().shape[0] for item in dataset_items]
+    spec_time_lengths = [item['spectrogram'].squeeze().shape[1] for item in dataset_items]
+    batch_text_encoded = torch.zeros(bs, max(text_time_lengths))
+    batch_spectrogram = torch.zeros(bs, max(spec_time_lengths), spec_ft_s)
+    for i, (item, text_length, spec_length) in enumerate(zip(
+            dataset_items, text_time_lengths, spec_time_lengths)):
+        batch_text_encoded[i, :text_length] = item['text_encoded'].squeeze()
+        batch_spectrogram[i, :spec_length, :] = item['spectrogram'].T.squeeze()
+    return {
+        'text_encoded': batch_text_encoded,
+        'text_encoded_length': torch.tensor(text_time_lengths),
+        'text': [item['text'] for item in dataset_items],
+        'spectrogram': batch_spectrogram,
+        'spectrogram_length': torch.tensor(spec_time_lengths),
+        'audio': [item['audio'] for item in dataset_items],
+        'duration': [item['duration'] for item in dataset_items]
+    }
+#     raise NotImplementedError
