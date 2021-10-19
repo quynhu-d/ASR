ad07e50030ed1ea7186a6437495892130c9c84ee
quynhu_d <dqnhu00@gmail.com>
2021-10-19 23:58:03 +0300

    pass valid_data_loader to trainer

--- train.py
+++ train.py
@@ -64,7 +64,7 @@ def main(config):
         config=config,
         device=device,
         data_loader=dataloaders["train"],
-        # valid_data_loader=None,
+        valid_data_loader=dataloaders["val"],
         lr_scheduler=lr_scheduler,
         len_epoch=config["trainer"].get("len_epoch", None)
     )
