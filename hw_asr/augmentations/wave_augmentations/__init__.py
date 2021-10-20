0a590c4228ae0d81dfa7834e1a001e09d9c3374d
quynhu_d <dqnhu00@gmail.com>
2021-10-20 17:20:59 +0300

    added wave augmentations from torch-audiomentations~=0.9.0

--- hw_asr/augmentations/wave_augmentations/__init__.py
+++ hw_asr/augmentations/wave_augmentations/__init__.py
@@ -1,9 +1,19 @@
+from hw_asr.augmentations.wave_augmentations.AddBackgroundNoise import AddBackgroundNoise
+from hw_asr.augmentations.wave_augmentations.AddColoredNoise import AddColoredNoise
+from hw_asr.augmentations.wave_augmentations.ApplyImpulseResponse import ApplyImpulseResponse
+from hw_asr.augmentations.wave_augmentations.BandPassFilter import BandPassFilter
 from hw_asr.augmentations.wave_augmentations.Gain import Gain
+from hw_asr.augmentations.wave_augmentations.HighPassFilter import HighPassFilter
+from hw_asr.augmentations.wave_augmentations.LowPassFilter import LowPassFilter
 from hw_asr.augmentations.wave_augmentations.PeakNormalization import PeakNormalization
+from hw_asr.augmentations.wave_augmentations.PitchShift import PitchShift
 from hw_asr.augmentations.wave_augmentations.PolarityInversion import PolarityInversion
+from hw_asr.augmentations.wave_augmentations.Shift import Shift
+from hw_asr.augmentations.wave_augmentations.ShuffleChannels import ShuffleChannels
 
 __all__ = [
+    "AddBackgroundNoise",
     "Gain",
     "PeakNormalization",
-    "PolarityInversion"
+    "PolarityInversion",
 ]
