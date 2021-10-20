50d66e190be2f4807358581fb49bee7a12b8c032
quynhu_d <dqnhu00@gmail.com>
2021-10-20 16:28:28 +0300

    added wave augmentations from torch-audiomentations~=0.4.0

--- hw_asr/augmentations/wave_augmentations/__init__.py
+++ hw_asr/augmentations/wave_augmentations/__init__.py
@@ -1,5 +1,9 @@
 from hw_asr.augmentations.wave_augmentations.Gain import Gain
+from hw_asr.augmentations.wave_augmentations.PeakNormalization import PeakNormalization
+from hw_asr.augmentations.wave_augmentations.PolarityInversion import PolarityInversion
 
 __all__ = [
-    "Gain"
+    "Gain",
+    "PeakNormalization",
+    "PolarityInversion"
 ]
