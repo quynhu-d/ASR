c4ee7d2db27a17fbb246ad86c34a2136202cb601
quynhu_d <dqnhu00@gmail.com>
2021-10-21 12:39:29 +0300

    commented out wave augmentations that are not checked

--- hw_asr/augmentations/wave_augmentations/__init__.py
+++ hw_asr/augmentations/wave_augmentations/__init__.py
@@ -1,27 +1,27 @@
 from hw_asr.augmentations.wave_augmentations.AddBackgroundNoise import AddBackgroundNoise
 from hw_asr.augmentations.wave_augmentations.AddColoredNoise import AddColoredNoise
 from hw_asr.augmentations.wave_augmentations.ApplyImpulseResponse import ApplyImpulseResponse
-from hw_asr.augmentations.wave_augmentations.BandPassFilter import BandPassFilter
+# from hw_asr.augmentations.wave_augmentations.BandPassFilter import BandPassFilter
 from hw_asr.augmentations.wave_augmentations.Gain import Gain
-from hw_asr.augmentations.wave_augmentations.HighPassFilter import HighPassFilter
-from hw_asr.augmentations.wave_augmentations.LowPassFilter import LowPassFilter
+# from hw_asr.augmentations.wave_augmentations.HighPassFilter import HighPassFilter
+# from hw_asr.augmentations.wave_augmentations.LowPassFilter import LowPassFilter
 from hw_asr.augmentations.wave_augmentations.PeakNormalization import PeakNormalization
 from hw_asr.augmentations.wave_augmentations.PitchShift import PitchShift
-from hw_asr.augmentations.wave_augmentations.PolarityInversion import PolarityInversion
+# from hw_asr.augmentations.wave_augmentations.PolarityInversion import PolarityInversion
 from hw_asr.augmentations.wave_augmentations.Shift import Shift
-from hw_asr.augmentations.wave_augmentations.ShuffleChannels import ShuffleChannels
+# from hw_asr.augmentations.wave_augmentations.ShuffleChannels import ShuffleChannels
 
 __all__ = [
     "AddBackgroundNoise",
     "AddColoredNoise",
     "ApplyImpulseResponse",
-    "BandPassFilter",
+    # "BandPassFilter",
     "Gain",
-    "HighPassFilter",
-    "LowPassFilter",
+    # "HighPassFilter",
+    # "LowPassFilter",
     "PeakNormalization",
     "PitchShift",
-    "PolarityInversion",
+    # "PolarityInversion",
     "Shift",
-    "ShuffleChannels"
+    # "ShuffleChannels"
 ]
