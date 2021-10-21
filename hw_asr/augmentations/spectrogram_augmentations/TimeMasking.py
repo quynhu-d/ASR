ad0f523f3f8b93d40a7e3195c9bc1319bac3b8c6
quynhu_d <dqnhu00@gmail.com>
2021-10-21 14:31:42 +0300

    removed transpose from spectrogram augmentations, freq-first elements are passed

--- hw_asr/augmentations/spectrogram_augmentations/TimeMasking.py
+++ hw_asr/augmentations/spectrogram_augmentations/TimeMasking.py
@@ -10,6 +10,5 @@ class TimeMasking(AugmentationBase):
         self._aug = torchaudio.transforms.TimeMasking(*args, **kwargs)
 
     def __call__(self, data: Tensor):
-        x = data.transpose(1, 2).unsqueeze(1)    # freq-first
-        print(x.shape)
-        return self._aug(x).squeeze(1).transpose(1, 2)    # time-first
+        x = data.unsqueeze(1)    # time-first
+        return self._aug(x).squeeze(1)    # time-first
